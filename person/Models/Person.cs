018ce7b4bb9d231dfd8c7284dd6a2c3267a92e5f
rdresch <rafaeldresch91@gmail.com>
2020-05-14 16:14:17 -0300

    Adicionar arquivos de projeto.

---
+++ person/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace person.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Addres { get; set; }
+        public long Gender { get; set; }
+
+    }
+}
